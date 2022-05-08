6800cc1421b721b0c58ef4449b1fac0132b56842
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-05-08 11:32:57 -0300

    avl

--- ArvBinaria/arvoreBinaria.c
+++ ArvBinaria/arvoreBinaria.c
@@ -8,3 +8,77 @@ struct NO{
     struct NO *dir;
 };
 
+ArvBin *cria_arvBin(){
+    ArvBin *raiz = (ArvBin*) = malloc(sizeof(ArvBin));
+    if(raiz != NULL){
+        *raiz = NULL;
+    }
+    return raiz;
+}
+
+void liberar_arvBin(ArvBin *raiz){
+    if(raiz == NULL){
+        return;
+    }
+    libera_NO(*raiz);
+    free(raiz);
+}
+
+void libera_NO(struct NO *no){
+    if(no == NULL){
+        return;
+    }
+    libera_NO(no->esq);
+    libera_NO(no->dir);
+    free(no);
+    no = NULL;
+}
+
+
+int vazia_arvBin(ArvBin *raiz){
+    if(raiz == NULL){
+        return 1;
+    }
+    if(*raiz == NULL){
+        return 1;
+    }
+    return 0;
+}
+
+int altura_arvBin(ArvBin *raiz){
+    if(raiz == NULL){
+        return 0;
+    }
+    if(*raiz == NULL){
+        return 0;
+    }
+    int alt_esq = altura_arvBin(&((*raiz)->esq));
+    int alt_dir = altura_arvBin(&((*raiz)->dir));
+
+    if(alt_esq > alt_dir){
+        return(alt_esq+1);
+    }else {
+        return(alt_dir+1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ArvBinaria/arvoreBinaria.h
+++ ArvBinaria/arvoreBinaria.h
@@ -2,4 +2,5 @@ typedef struct NO *ArvBin;
 
 ArvBin *cria_arvBin();
 
+void liberar_arvBin(ArvBin *raiz);
 
