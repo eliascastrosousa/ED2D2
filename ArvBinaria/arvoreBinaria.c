702e811cfac56b47edf8524db768434fa3692fa1
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-05-07 22:05:33 -0300

    commit

--- ArvBinaria/arvoreBinaria.c
+++ ArvBinaria/arvoreBinaria.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvoreBinaria.h"
+
+struct NO{
+    int info;
+    struct NO *esq;
+    struct NO *dir;
+};
 
--- ArvBinaria/arvoreBinaria.h
+++ ArvBinaria/arvoreBinaria.h
@@ -1,1 +1,5 @@
+typedef struct NO *ArvBin;
+
+ArvBin *cria_arvBin();
+
 
